d0df381b71ef28f46d312335884c8cca13a72a87
dimisavva <119276394+dimisavva@users.noreply.github.com>
2022-12-20 14:58:47 -0800

    cards flip when clicked, get value when second card is clicked

--- js/app.js
+++ js/app.js
@@ -8,12 +8,21 @@
 
 // ]
 
-const cards = document.querySelectorAll(".cards");
+const cards = document.querySelectorAll(".card");
 
-function flipCard(e)
-console.log(e.target);
+let card1, card2;
+
+function flipCard(e) { 
+  let clickedCard = e.target; //collecting user's clicked card
+  clickedCard.classList.add("flip");
+if(!card1){
+  //return the cardOne value to clickCard
+  return card1 = clickedCard
+}
+  card2 = clickedCard;
+  console.log(card1, card2)
+}
 
 cards.forEach(card => { //this adds click event to all cards
   card.addEventListener("click", flipCard);
-
-})
+});
