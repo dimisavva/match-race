7b7ea419ec75646c9d61146e2640d2cc12a75b4a
dimisavva <119276394+dimisavva@users.noreply.github.com>
2022-12-20 15:03:21 -0800

    disable same card being recorder twice

--- js/app.js
+++ js/app.js
@@ -14,13 +14,15 @@ let card1, card2;
 
 function flipCard(e) { 
   let clickedCard = e.target; //collecting user's clicked card
-  clickedCard.classList.add("flip");
-if(!card1){
-  //return the cardOne value to clickCard
-  return card1 = clickedCard
-}
-  card2 = clickedCard;
-  console.log(card1, card2)
+  if(clickedCard !== card1){
+    clickedCard.classList.add("flip");
+    if(!card1){
+      //return the cardOne value to clickCard
+      return card1 = clickedCard
+    }
+      card2 = clickedCard;
+      console.log(card1, card2)
+  }
 }
 
 cards.forEach(card => { //this adds click event to all cards
