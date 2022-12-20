bcda6e9894e0ed02a5db7521c30f6fd8e0a15cf6
dimisavva <119276394+dimisavva@users.noreply.github.com>
2022-12-20 09:45:11 -0800

    add click event to all cards

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-const cardArray = [
-  { name: 'Ru-Card'
-    img: 'images/Ru.jpg'
-  },
-  { name: 'Ru-Card'
-  img: 'images/Ru.jpg'
-},
+// const cardArray = [
+//   { name: 'Ru-Card'
+//     img: 'images/Ru.jpg'
+//   },
+//   { name: 'Ru-Card'
+//   img: 'images/Ru.jpg'
+// },
 
-]
+// ]
+
+const cards = document.querySelectorAll(".cards");
+
+function flipCard(e)
+console.log(e.target);
+
+cards.forEach(card => { //this adds click event to all cards
+  card.addEventListener("click", flipCard);
+
+})
